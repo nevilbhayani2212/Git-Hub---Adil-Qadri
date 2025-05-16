d5a8008ed3cf1096b557ebd6ce39b66b33dfe284
NevilFlexion <nevilbhayani.flexion@gmail.com>
2025-05-16 11:00:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3026,4 +3026,4 @@ def get_top_selling_products(db: Session = Depends(get_db)):
     
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8001, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=10000, reload=True)
